bebb2520065c9dd56c4824fbfaaf8566086bacba
Jake Heft <jakeheft@gmail.com>
2020-10-23 11:05:36 -0600

    Update test to correct mistakes

--- spec/features/amusement_parks/show_spec.rb
+++ spec/features/amusement_parks/show_spec.rb
@@ -4,23 +4,27 @@ describe "When I visit an amusement park's show page" do
   it "I see its name, admission price, its rides, and average thrill rating" do
     adventureland = AmusementPark.create(
       name: "Adventureland",
-      price: 25.0
+      price: 25.00
     )
     comet = Ride.create(
       name: "Vomit Comet",
-      rating: 10
+      rating: 10,
+      amusement_park_id: adventureland.id
     )
     coaster = Ride.create(
       name: "Generic Roller Coaster",
-      rating: 4
+      rating: 4,
+      amusement_park_id: adventureland.id
     )
     spinny = Ride.create(
       name: "Spinny Pods",
-      rating: 6
+      rating: 6,
+      amusement_park_id: adventureland.id
     )
     visit "/amusement_parks/#{adventureland.id}"
-
+save_and_open_page
     expect(page).to have_content(adventureland.name)
+    expect(page).to have_content("Admissions: $#{adventureland.price}")
     expect(page).to have_content(comet.name)
     expect(page).to have_content(coaster.name)
     expect(page).to have_content(spinny.name)
