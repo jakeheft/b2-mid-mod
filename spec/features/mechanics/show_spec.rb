972ffaf81dd3673f0fb655c9469bbf26258548f2
Jake Heft <jakeheft@gmail.com>
2020-10-23 11:20:03 -0600

    Add test to see mechanic info on show and field form

---
+++ spec/features/mechanics/show_spec.rb
@@ -0,0 +1,51 @@
+require 'rails_helper'
+
+describe "When I visit a mechanics show page I see the mechanic's info" do
+  it "I can fill out a form to add a ride_id from the db to the mechanics list and upon submitting I see that ride listed on this show page" do
+    adventureland = AmusementPark.create(
+      name: "Adventureland",
+      price: 25.00
+    )
+    comet = Ride.create(
+      name: "Vomit Comet",
+      rating: 10,
+      amusement_park_id: adventureland.id
+    )
+    coaster = Ride.create(
+      name: "Generic Roller Coaster",
+      rating: 4,
+      amusement_park_id: adventureland.id
+    )
+    spinny = Ride.create(
+      name: "Spinny Pods",
+      rating: 6,
+      amusement_park_id: adventureland.id
+    )
+    roy = Mechanic.create(
+      name: "Roy Rogers",
+      experience: 22
+    )
+    MechanicRide.create(
+      mechanic_id: roy.id,
+      ride_id: comet.id
+    )
+    MechanicRide.create(
+      mechanic_id: roy.id,
+      ride_id: coaster.id
+    )
+
+    visit "mechanics/#{roy.id}"
+
+    expect(page).to have_content("Mechanic: #{roy.name}")
+    expect(page).to have_content("Years of Experience: #{roy.experience}")
+    expect(page).to have_content(comet.name)
+    expect(page).to have_content(coaster.name)
+    expect(page).to_not have_content(spinny.name)
+
+    fill_in :add_ride, with: spinny.id
+    click_button "Submit"
+
+    expect(current_path).to eq("mechanics/#{roy.id}")
+    expect(page).to have_content(spinny.name)
+  end
+end
