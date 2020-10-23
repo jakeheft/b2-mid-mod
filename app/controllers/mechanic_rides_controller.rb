f07fe58f489da21c6c1b5f25f6b13068102216d0
Jake Heft <jakeheft@gmail.com>
2020-10-23 11:42:20 -0600

    Add functionality to see a mechanic show page and add a ride to their workload

---
+++ app/controllers/mechanic_rides_controller.rb
@@ -0,0 +1,9 @@
+class MechanicRidesController < ApplicationController
+  def create
+    mech_ride = MechanicRide.create(
+      mechanic_id: params[:mechanic_id],
+      ride_id: params[:ride_id]
+    )
+    redirect_to "/mechanics/#{params[:mechanic_id]}"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/mechanics', to: 'mechanics#index'
+  get '/mechanics/:mechanic_id', to: 'mechanics#show'
+
+  post '/mechanic_rides/:mechanic_id', to: 'mechanic_rides#create'
 
   get '/amusement_parks/:park_id', to: 'amusement_parks#show'
 end
