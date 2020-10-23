276d51f648c6becbf395cfaa4c4d016b07dc6069
Jake Heft <jakeheft@gmail.com>
2020-10-23 11:56:12 -0600

    Add tests to alphabetize rides

--- spec/features/mechanics/show_spec.rb
+++ spec/features/mechanics/show_spec.rb
@@ -49,3 +49,49 @@ describe "When I visit a mechanics show page I see the mechanic's info" do
     expect(page).to have_content(spinny.name)
   end
 end
+
+describe "On the mechanic show page" do
+  it "Rides are shown alphabetically" do
+    adventureland = AmusementPark.create(
+      name: "Adventureland",
+      price: 25.00
+    )
+    comet = Ride.create(
+      name: "Vomit Comet",
+      rating: 10,
+      amusement_park_id: adventureland.id
+    )
+    coaster = Ride.create(
+      name: "Generic Roller Coaster",
+      rating: 4,
+      amusement_park_id: adventureland.id
+    )
+    spinny = Ride.create(
+      name: "Spinny Pods",
+      rating: 6,
+      amusement_park_id: adventureland.id
+    )
+    roy = Mechanic.create(
+      name: "Roy Rogers",
+      experience: 22
+    )
+    MechanicRide.create(
+      mechanic_id: roy.id,
+      ride_id: comet.id
+    )
+    MechanicRide.create(
+      mechanic_id: roy.id,
+      ride_id: coaster.id
+    )
+    MechanicRide.create(
+      mechanic_id: roy.id,
+      ride_id: spinny.id
+    )
+
+    visit "mechanics/#{roy.id}"
+save_and_open_page
+    expect(page.all('li')[0]).to have_content(coaster.name)
+    expect(page.all('li')[1]).to have_content(spinny.name)
+    expect(page.all('li')[2]).to have_content(comet.name)
+  end
+end
--- spec/models/mechanic_spec.rb
+++ spec/models/mechanic_spec.rb
@@ -5,4 +5,46 @@ describe Mechanic, type: :model do
     it { should have_many :mechanic_rides }
     it { should have_many(:rides).through(:mechanic_rides) }
   end
+
+  describe "instance methods" do
+    it "#alphabetize_rides" do
+      adventureland = AmusementPark.create(
+        name: "Adventureland",
+        price: 25.00
+      )
+      comet = Ride.create(
+        name: "Vomit Comet",
+        rating: 10,
+        amusement_park_id: adventureland.id
+      )
+      coaster = Ride.create(
+        name: "Generic Roller Coaster",
+        rating: 4,
+        amusement_park_id: adventureland.id
+      )
+      spinny = Ride.create(
+        name: "Spinny Pods",
+        rating: 6,
+        amusement_park_id: adventureland.id
+      )
+      roy = Mechanic.create(
+        name: "Roy Rogers",
+        experience: 22
+      )
+      MechanicRide.create(
+        mechanic_id: roy.id,
+        ride_id: comet.id
+      )
+      MechanicRide.create(
+        mechanic_id: roy.id,
+        ride_id: coaster.id
+      )
+      MechanicRide.create(
+        mechanic_id: roy.id,
+        ride_id: spinny.id
+      )
+
+      expect(roy.alphabetize_rides).to eq([coaster, spinny, comet])
+    end
+  end
 end
